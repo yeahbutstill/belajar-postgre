51ec013e3f346be5d6fb39db30cadb922b175fd3
yeahbutstill <mayadani28@gmail.com>
2023-04-14 23:53:11 +0700

    select all specific

---
+++ sql/01-select/select-all-but-specific.sql
@@ -0,0 +1,2 @@
+SELECT department_id, department_name FROM departments;
+select "public".departments.department_name FROM departments;
--- sql/01-select/select-all-data.sql
+++ sql/01-select/select-all-data.sql
@@ -1,3 +1,1 @@
 SELECT * FROM departments;
-SELECT department_id, department_name FROM departments;
-select "public".departments.department_name FROM departments;
