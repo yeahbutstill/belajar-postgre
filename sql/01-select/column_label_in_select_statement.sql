557c552f21e669117b5a96a9068c50f703088eb7
yeahbutstill <mayadani28@gmail.com>
2023-04-21 14:47:20 +0700

    escape characters

--- sql/01-select/column_label_in_select_statement.sql
+++ sql/01-select/column_label_in_select_statement.sql
@@ -2,3 +2,9 @@ select department_id as kode_divisi,
        department_name nama_dapartment,
        manager_id as "Kode manager"
 from departments;
+
+select '*' bintang,
+        E'()' kurung,
+        E'\\' slash,
+        E'baris pertama \n baris kedua' newline,
+        E'awal\t setelah' tabspace;
---
+++ sql/01-select/using_special_escape_chracters.sql
@@ -0,0 +1,5 @@
+select '*' bintang,
+        E'()' kurung,
+        E'\\' slash,
+        E'baris pertama \n baris kedua' newline,
+        E'awal\t setelah' tabspace;
