9f2535382ef789f298b4a611908cdefca4984300
yeahbutstill <mayadani28@gmail.com>
2023-04-21 15:07:29 +0700

    elimination of duplication rows

--- sql/01-select/elimination_of_duplicate_rows.sql
+++ sql/01-select/elimination_of_duplicate_rows.sql
@@ -1,2 +1,4 @@
 select distinct (job_id, manager_id), job_id, manager_id from employees;
 select distinct job_id from employees;
+
+select distinct on (department_id, job_id) department_id, job_id from employees;
