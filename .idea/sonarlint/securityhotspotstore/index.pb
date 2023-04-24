d4a99cdf0909547e43e0567011e645048168511a
yeahbutstill <mayadani28@gmail.com>
2023-04-25 06:43:04 +0700

    using build string

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,3 @@
+
+\
+,sql/01-select/using_comperation_operator.sql,2/7/275a515b013078dc43dfd1e5021b26019c5f6dca
---
+++ sql/using_build_in_string_function.sql
@@ -0,0 +1,11 @@
+select substring('Dani Setiawan' from 1 for 3) as substring_from,
+    substr('Dani Setiawan', 1, 5) as "substr",
+    lower('Ini Adalah Text BESAR dan Kecil') as "lower",
+    upper('Ini Adalah Text BESAR dan Kecil') as "upper",
+    initcap('Ini Adalah Text BESAR dan Kecil') as "initcap",
+    trim(both ' ' from ' ini text ada spacenya ') as "trim both",
+    trim(trailing ' ' from ' ini text ada spacenya ') as "trim trailing",
+    reverse('dani') as "reverse",
+    length('dani setiawan') as "length",
+    concat('yeahbutstill', ' : ', 'Dani Setiawan') as "concat",
+    ascii('D') as "ascii";
