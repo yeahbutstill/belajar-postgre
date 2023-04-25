0325e25a69e040e17ee069a7f4f96fac3d5433a5
yeahbutstill <mayadani28@gmail.com>
2023-04-26 01:43:27 +0700

    build math

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,5 @@
 
 \
 ,sql/01-select/using_comperation_operator.sql,2/7/275a515b013078dc43dfd1e5021b26019c5f6dca
+X
+(sql/01-select/using_logical_operator.sql,d/b/db9e93b20ab649d447e9dfd14345df908e537822
---
+++ sql/01-select/using_build_in_math_function.sql
@@ -0,0 +1,9 @@
+select abs(-10) as "absolut",
+    div(10, 3) as "division",
+    mod(5, 2) as "mod",
+    power(2, 3) as "power",
+    round(5.451234, 2) as "round scale2",
+    round(5.43) as "round",
+    round(5.6) as "roundup",
+    floor(5.45234) as "floor",
+    floor(5.6) as "floor2";
