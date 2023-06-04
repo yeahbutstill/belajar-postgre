26a440dc3f045a2a17cc46e96e22bb6b7f98b4e2
yeahbutstill <mayadani28@gmail.com>
2023-06-04 14:29:04 +0700

    oreder by

---
+++ sql/01-select/basic_use_of_order_by_clause.sql
@@ -0,0 +1,11 @@
+SELECT location_id, department_id, department_name
+FROM departments
+ORDER BY location_id;
+
+SELECT location_id, department_id, department_name
+FROM departments
+ORDER BY location_id DESC;
+
+SELECT location_id, department_id, department_name
+FROM departments
+ORDER BY department_name DESC;
