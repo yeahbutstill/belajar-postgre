9665ecac0f8d713768697629b59f1fbe2822b836
yeahbutstill <mayadani28@gmail.com>
2023-06-08 18:20:10 +0700

    using group sets

--- sql/01-select/using_grouping_set.sql
+++ sql/01-select/using_grouping_set.sql
@@ -1,9 +1,17 @@
+SELECT department_id,
+       job_id,
+       COUNT(*) as count_employees_by_job,
+       SUM(salary) as salary_group_by_job
+FROM employees
+GROUP BY GROUPING SETS(job_id, department_id, (job_id, department_id))
+ORDER BY COUNT(*) DESC;
+
 SELECT manager_id,
        department_id,
        COUNT(*) as count_employees_by_department,
        SUM(salary) as salary_group_by_department
 FROM employees
-GROUP BY GROUPING SETS((manager_id), (department_id))
+GROUP BY GROUPING SETS(manager_id, department_id)
 ORDER BY COUNT(*) DESC;
 
 -- tanpa menggunakan grouping sets
