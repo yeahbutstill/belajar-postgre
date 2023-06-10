9eb0e0a04b17f1ad0fe4cc580b97e6d94ca43a25
yeahbutstill <mayadani28@gmail.com>
2023-06-10 15:28:28 +0700

    inline view format

---
+++ sql/01-select/using_subquery_inline_view_form.sql
@@ -0,0 +1,12 @@
+SELECT emp.employee_id,
+       emp.first_name,
+       emp.salary,
+       func.rata_rata,
+       func.min,
+       func.max
+FROM employees AS emp,
+     (SELECT ROUND(AVG(j.max_salary), 0) AS rata_rata,
+             MIN(j.max_salary) AS min,
+             MAX(j.max_salary) AS max
+             FROM jobs AS j) AS func
+WHERE emp.salary >= func.rata_rata;
