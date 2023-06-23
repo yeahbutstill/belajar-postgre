83f3f81bee8a46157d834f8b19e5d31ede7bb4e5
yeahbutstill <mayadani28@gmail.com>
2023-06-23 19:39:07 +0700

    using except queries

--- sql/01-select/combine-queries.sql
+++ sql/01-select/combine-queries.sql
@@ -54,3 +54,31 @@ select
            ) as data2
 order by column1 desc;
 
+select employee_id, first_name, salary, commission_pct, department_id
+from employees
+where department_id = 90
+union
+select employee_id, first_name, salary, commission_pct, department_id
+from employees
+where department_id in (10, 80)
+except
+select employee_id, first_name, salary, commission_pct, employees.department_id
+from employees
+where employees.salary < 8800;
+
+
+select * from (values (1, 'yeahbutstill', 'Dani Setiawan', true),
+                      (2, 'naruto', 'Naruto Maki', false),
+                      (2, 'naruto', 'Naruto Maki', false),
+                      (3, 'sasuke', 'Sasasusuke', true)) as data1
+except
+select * from (values (1, 'yeahbutstill', 'Dani Setiawan', true),
+                      (3, 'sasuke', 'Sasasusuke', true)) as data2;
+
+select * from (values (1, 'yeahbutstill', 'Dani Setiawan', true),
+                      (2, 'naruto', 'Naruto Maki', false),
+                      (2, 'naruto', 'Naruto Maki', false),
+                      (3, 'sasuke', 'Sasasusuke', true)) as data1
+except all
+select * from (values (1, 'yeahbutstill', 'Dani Setiawan', true),
+                      (3, 'sasuke', 'Sasasusuke', true)) as data2;
