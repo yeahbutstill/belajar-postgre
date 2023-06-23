7d471e66d3f530e62abf272f361a5ebef0fa14bb
yeahbutstill <mayadani28@gmail.com>
2023-06-23 19:02:32 +0700

    using combination

--- sql/01-select/combine-queries.sql
+++ sql/01-select/combine-queries.sql
@@ -1,3 +1,4 @@
+-- ingat jumlah kolom harus sama persis dari query 1 dan query 2, posisi, type data dan nama kolom harus sama persis juga.
 select
 * from (values(1, 'yeahbutstill', 'Dani Setiawan', true),
               (2, 'naruto', 'Naruto Maki', false),
@@ -23,3 +24,33 @@ select
                   (5, 'lestari', 'Winda', null)
            ) as data2
 order by column1 desc;
+
+
+select
+    * from (values(1, 'yeahbutstill', 'Dani Setiawan', true),
+                  (2, 'naruto', 'Naruto Maki', false),
+                  (3, 'sasuke', 'Sasasusuke', null),
+                  (4, 'triyanti', 'Maoy', false)
+           ) as data1
+intersect distinct
+select
+    * from (values(1, 'yeahbutstill', 'Dani Setiawan', true),
+                  (4, 'triyanti', 'Maoy', false),
+                  (5, 'lestari', 'Winda', null)
+           ) as data2
+order by column1 desc;
+
+select
+    * from (values(1, 'yeahbutstill', 'Dani Setiawan', true),
+                  (2, 'naruto', 'Naruto Maki', false),
+                  (3, 'sasuke', 'Sasasusuke', null),
+                  (4, 'triyanti', 'Maoy', false)
+           ) as data1
+intersect all
+select
+    * from (values(1, 'yeahbutstill', 'Dani Setiawan', true),
+                  (4, 'triyanti', 'Maoy', false),
+                  (5, 'lestari', 'Winda', null)
+           ) as data2
+order by column1 desc;
+
